265474c638f0ca916ca4a7aa1abd69b617994c1a
lokesh12510 <lokeshkrishna50@gmail.com>
2022-07-04 21:30:57 +0530

    delete fix

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,32 +1,41 @@
 import axios from "axios";
 import { BASE_URL } from "../Constant";
+import { logout } from "../redux/actions/Auth.actions";
 import { store } from "../redux/Store";
 
 const HEADERS = {
-  "Content-Type": "application/json",
+    "Content-Type": "application/json",
 };
 
 export const HttpClient = axios.create({
-  baseURL: `${BASE_URL}`,
-  headers: {
-    ...HEADERS,
-  },
-  withCredentials: true,
+    baseURL: `${BASE_URL}`,
+    headers: {
+        ...HEADERS,
+    },
+    withCredentials: true,
 });
 
 HttpClient.interceptors.request.use((config) => {
-  const state = store.getState();
-  let token = state.auth.accessToken;
-  config.headers.Authorization = `Bearer ${token}`;
-  return config;
+    const state = store.getState();
+    let token = state.auth.accessToken;
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
 });
 
 HttpClient.interceptors.response.use(
-  (res) => {
-    return Promise.resolve(res);
-  },
-  (error) => {
-    console.log(error);
-    return Promise.reject(error);
-  }
+    (res) => {
+        console.log(res)
+        return Promise.resolve(res);
+    },
+    (error) => {
+        console.log(error);
+        if (error.response && error.respones.status) {
+            const resStatus = error.respones.status;
+            if (resStatus === 401 || resStatus === 403) {
+                store.dispatch(logout())
+                window.location.reload()
+            }
+        }
+        return Promise.reject(error);
+    }
 );
